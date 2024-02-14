f47313adae92e0e9da491138876bdf4f7f798a61
Clarence-Coronel <clarence.coronell@gmail.com>
2024-02-14 14:05:18 +0800

    Initial react and tailwindcss setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <div className="w-screen h-screen bg-green-400 flex justify-center items-center text-3xl font-bold text-white">
+        Hello World!
+      </div>
+  )
+}
+
+export default App
