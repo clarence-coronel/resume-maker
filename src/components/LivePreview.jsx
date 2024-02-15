497eff3548101ae43a486929e75995005ea118c2
Clarence-Coronel <clarence.coronell@gmail.com>
2024-02-15 11:52:00 +0800

    Remove insert sample resume btn

--- src/components/LivePreview.jsx
+++ src/components/LivePreview.jsx
@@ -4,7 +4,7 @@ import ResumeExperience from "./ResumeExperience"
 
 export default function LivePreview({ personalDetailsObj }){
     return (
-        <div id="livePreview" className="h-fit p-2 lg:p-7">
+        <div id="livePreview" className="h-fit p-2 lg:p-7 overflow-hidden">
             <div class="aspect-[8/11] border-2 border-gray-200shadow">
                 <ResumeHeader name={personalDetailsObj.name} email={personalDetailsObj.email} phone={personalDetailsObj.phone} address={personalDetailsObj.address} />
                 <div className="p-3 lg:px-20 lg:py-10 flex flex-col gap-y-5">
